c5e8675a0f5b94caa17c9399c043366012526352
Harris Temuri <harris.temuri@gmail.com>
2026-02-03 00:31:05 -0500

    add hover on badcode link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import TerminalPlayer from "./player";
 import Link from "next/link";
 import Image from "next/image";
-import { LinkedinIcon, MountainSnowIcon } from "lucide-react";
+import { ChevronRight, ChevronRightIcon, GithubIcon, LinkedinIcon, MountainSnowIcon } from "lucide-react";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
 
@@ -14,24 +16,43 @@ export default function Home() {
             hello
           </div>
           <div className="grid grid-rows-10 flex-col col-span-8 grow gap-4">
-            <div className="hover:border-orange-300 row-span-1 flex border border-gray-500 items-center px-4 py-2 gap-5">
-              <div className="flex gap-3">
-                <Avatar>
+            <div className="hover:border-orange-300 row-span-1 flex border border-gray-500 items-center pl-2 pr-4 py-2 gap-6">
+              <div className="flex gap-1 items-center">
+                {/* <Avatar>
                   <AvatarImage src={"https://avatars.githubusercontent.com/u/51043768?v=4"} alt="@harristemuri" className="" />
                   <AvatarFallback>HT</AvatarFallback>
-                </Avatar>
-                <code className="text-md cursor-default text-cyan-600 dark:text-cyan-300 mt-1.5">Harris Temuri - Platform Engineer</code>
+                </Avatar> */}
+                <ChevronRightIcon width={20} />
+                <code className="text-md cursor-default text-cyan-600 dark:text-cyan-300 mt-0.5">Harris Temuri - Platform Engineer</code>
               </div>
               <div className="grow" />
-              <Link href={"https://htemuri.github.io/0xBADC0DE/"} target="_blank">
-                <MountainSnowIcon width={20} className="ml-1" />
-              </Link>
+              <HoverCard openDelay={10} closeDelay={100}>
+                <HoverCardTrigger asChild>
+                  <button >
+                    <Link href={"https://htemuri.github.io/0xBADC0DE/"} target="_blank">
+                      <MountainSnowIcon width={20} />
+                    </Link>
+                  </button>
+                </HoverCardTrigger>
+                <HoverCardContent className="flex w-96 flex-col gap-0.5">
+                  <Link href={"https://htemuri.github.io/0xBADC0DE/"} target="_blank" className="font-semibold text-cyan-600 dark:text-cyan-300">0xBADC0DE</Link>
+                  <div>Blog where I document the things I've learned.</div>
+                  <div className="text-muted-foreground mt-1 text-xs">
+                    <iframe src="https://htemuri.github.io/0xBADC0DE/" width="350px" height="400px" ></iframe>
+                  </div>
+                </HoverCardContent>
+              </HoverCard>
+              {/* <Button asChild variant={"ghost"} className="pl-1"> */}
               <Link href={"https://linkedin.com/in/harris-temuri"} target="_blank">
-                <LinkedinIcon width={20} className="ml-1" />
+                <LinkedinIcon width={20} className="" />
               </Link>
+              {/* </Button> */}
+              {/* <Button asChild variant={"ghost"}> */}
               <Link href={"https://github.com/htemuri"} target="_blank">
-                <Image src={"/github.svg"} alt="github icon" width={20} height={20} />
+                {/* <GithubIcon width={25} /> */}
+                <Image src={"/github.svg"} alt="github icon" className="p-0 m-0" width={20} height={20} />
               </Link>
+              {/* </Button> */}
             </div>
             <div className="hover:border-orange-300 row-span-9 border border-gray-500 grow w-full">
               <TerminalPlayer />
