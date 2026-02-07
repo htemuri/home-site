3e053cfda5fe7735084ddb4ed852e9d3b058c7e0
Harris Temuri <harris.temuri@gmail.com>
2026-02-06 20:44:26 -0500

    full intro typed out is long, so animating bulk of text to appear instantly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import { CodeBlock } from "@/components/animate-ui/primitives/animate/code-block";
+import { Slide } from "@/components/animate-ui/primitives/effects/slide";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 export default function Home() {
+  const [showWhoami, setShowWhoami] = useState(false);
+
   const introductionCode = `[2mStarting session…[0m
 [1;32mConnected.[0m
 
 [1m[1;32m$[0m whoami[0m
-Hi, I’m [1;36mHarris[0m![0m
+
+`;
+  const whoami = `Hi, I’m [1;36mHarris[0m![0m
 
 I tend to approach engineering as a long process of learning rather than a fixed skill set. I’m most interested in computer science fundamentals - the ideas and mechanisms that stay useful even as tools, technologies, and roles change.
 
@@ -18,17 +27,24 @@ Thanks for visiting!
 [38;5;214m  |\\__/,|[0m   [38;5;214m(\`\\[0m
 [38;5;214m_.|[32mo[0m [32mo[0m  [38;5;214m|_[0m   [38;5;214m) )[0m
 [38;5;239m---------------------[0m
-
 `;
+
   return (
-    <div>
+    <div className="flex flex-col ml-2 pt-3 gap-1">
       <CodeBlock
-        className="ml-2 mt-1"
         code={introductionCode}
         lang="ansi"
         duration={5000}
         writing={true}
+        onDone={() => {
+          setTimeout(() => setShowWhoami(true), 200);
+        }}
       />
+      {showWhoami && (
+        <Slide>
+          <CodeBlock code={whoami} lang="ansi" />
+        </Slide>
+      )}
     </div>
   );
 }
